5898028d37c63499beef01b56333efdf074cfcaf
Diego Brito <dibbrito@gmail.com>
2026-07-17 12:04:48 -0300

    chore: add AWS Lambda functions for image processing

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,13 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.36.0"
+    }
+  }
+}
 locals {
+    env_variables               = { STAGE = "local" }
     images_bucket         = "localstack-thumbnails-app-images"
     image_resized_bucket  = "localstack-thumbnails-app-resized"
 }
@@ -11,4 +20,37 @@ resource "aws_s3_bucket" "images_bucket" {
 
 resource "aws_s3_bucket" "image_resized_bucket" {
   bucket = local.image_resized_bucket
+}
+
+resource "aws_lambda_function" "presign_lambda" {
+  function_name = "presign"
+  handler       = "handler.handler"
+  runtime       = "python3.11"
+  timeout       = 10
+  role          = ""
+  environment {
+    variables = local.env_variables
+  }
+}
+
+resource "aws_lambda_function" "list_lambda" {
+  function_name = "list"
+  handler       = "handler.handler"
+  runtime       = "python3.11"
+  timeout       = 10
+  role          = ""
+  environment {
+    variables = local.env_variables
+  }
+}
+
+resource "aws_lambda_function" "resizer_lambda" {
+  function_name = "resizer"
+  handler       = "handler.handler"
+  runtime       = "python3.11"
+  timeout       = 10
+  role          = ""
+  environment {
+    variables = local.env_variables
+  }
 }
